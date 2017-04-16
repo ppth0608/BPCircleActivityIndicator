2bb2b7876ff375cea247ccd9e95a25922f57d87e
taehyun.park <taehyun.park@navercorp.com>
2017-04-16 14:53:40 +0900

    initial commit

---
+++ Example/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Example
+//
+//  Created by Naver on 2017. 4. 16..
+//  Copyright © 2017년 BPCircleActivityIndicator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+}
